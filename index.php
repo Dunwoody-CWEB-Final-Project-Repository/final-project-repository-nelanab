da8d3d65cf1cdc716fb1662ffafb092497970613
Ana Nelson <nelanab@dunwoody.edu>
2021-03-10 15:03:33 -0600

    finalizing navigation

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
         <title>Reffle - Home</title>
         <link rel="stylesheet" href="libs/bootstrap-4.0.0/dist/css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/overrides.css" />
-        <link rel="stylesheet" href="css/navigation.css" />
         <link rel="stylesheet" href="css/index.css" />
+        <link rel="stylesheet" href="css/navigation.css" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;800;900&display=swap" rel="stylesheet">
     </head>
     
